368ea5cfb45e8b9be2ba5f7d9ca4ba0a0b52c436
NahianNahin <nahian1927040@stud.kuet.ac.bd>
2022-11-24 17:56:36 +0600

    Add Mongodb Database

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
 
 //MiddleWare
 app.use(cors());
 app.use(express.json());
 
 
-const app = express();
-const port = process.env.PORT || 5000;
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4k7t9co.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const usersCollection = client.db("TV-Shop").collection("users");
+
+
+
+    } finally {
+    //   await client.close();
+    }
+  }
+  run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send('TV Shop server is running')
