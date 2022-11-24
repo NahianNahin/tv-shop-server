7e832c876898559a84a21bb046ee6d8200a408c5
NahianNahin <nahian1927040@stud.kuet.ac.bd>
2022-11-25 02:51:34 +0600

    Post products API complete

--- index.js
+++ index.js
@@ -19,32 +19,39 @@ async function run() {
     try {
         const usersCollection = client.db("TV-Shop").collection("users");
         const categoriesCollection = client.db("TV-Shop").collection("Categories");
+        const productsCollection = client.db("TV-Shop").collection("products");
+
+
         //Post User Details
-        app.post('/users', async(req, res) => {
+        app.post('/users', async (req, res) => {
             const user = req.body;
             const result = await usersCollection.insertOne(user);
             res.send(result);
-          })
+        })
 
-          // Get Categories 
-          app.get('/categories', async(req, res) => {
+        // Get Categories 
+        app.get('/categories', async (req, res) => {
             const query = {};
             const result = await categoriesCollection.find(query).toArray();
             res.send(result);
-          })
-
-
+        })
+        // Post Product
+        app.post('/products', async(req, res) => {
+            const product = req.body;
+            const result = await productsCollection.insertOne(product);
+            res.send(result);
+        })
     } finally {
-    //   await client.close();
+        //   await client.close();
     }
-  }
-  run().catch(console.dir);
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-  res.send('TV Shop server is running')
+    res.send('TV Shop server is running')
 })
 
 app.listen(port, () => {
-  console.log(`TV Shop server is listening on port ${port}`)
+    console.log(`TV Shop server is listening on port ${port}`)
 })
